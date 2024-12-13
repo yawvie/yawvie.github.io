58f0410c9c3824bd19c84eb1f92203050e9e0401
yawvie <105031782+yawvie@users.noreply.github.com>
2024-12-12 21:25:57 -0700

    deploy addition

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,11 +1,11 @@
 export const mainMenu = [
-     {link:"yawvie.github.io/#", content:"Home", icon:"lucide:home"},
-     {link:"yawvie.github.io/#about", content:"About", icon:"octicon:person-24"},
-     {link:"yawvie.github.io/#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
+     {link:"#", content:"Home", icon:"lucide:home"},
+     {link:"#about", content:"About", icon:"octicon:person-24"},
+     {link:"#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
      //{link:"#blog", content:"Blog", icon:"mdi:blog"}
 ]
 export const menuBlog = [
-     {link:"yawvie.github.io/", content:"Home", icon:"lucide:home"},
-     {link:"yawvie.github.io/#about", content:"About", icon:"octicon:person-24"},
-     {link:"yawvie.github.io/#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
+     {link:"/", content:"Home", icon:"lucide:home"},
+     {link:"/#about", content:"About", icon:"octicon:person-24"},
+     {link:"/#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
 ]
